036145fc87034f99a92cc098ac5b69bf7a05eb42
jhionathan <jhionathanb@outlook.com>
2024-12-11 17:09:44 -0300

    gsap

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,19 +1,45 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { MapPinCheckInside } from "lucide-react"
 import Link from "next/link"
+import { useEffect } from "react"
+import { useRef } from "react"
+import { gsap } from "gsap"
 
 interface ButtonProps {
     title: string
     href: string
     className?: string
+
 }
 
-export default function ButtonUse( { title, href, className }: ButtonProps ) {
+export default function ButtonUse({ title, href, className }: ButtonProps) {
+
+    const buttonRef = useRef(null)
+    useEffect(() => {
+        gsap.fromTo(buttonRef.current, {
+            y: 100,
+            opacity: 0,
+            duration: 1,
+            ease: "bounce.out",
+        },
+            {
+                y: 0,
+                opacity: 1,
+                duration: 1,
+                ease: "bounce.out",
+
+            })
+    })
+
     return (
-        <Button variant={"outline"}
-        className={`absolute  bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100 ${className}`}>
-            <MapPinCheckInside className="h-4 w-4 text-white" />
-            <Link href={href} className="text-white text-lg">{title}</Link>
-        </Button>
+        <Link href={href}>
+            <Button variant={"outline"}
+                className={`absolute text-white text-lg bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100 hover:text-white ${className}`}
+                ref={buttonRef}>
+                <MapPinCheckInside className="h-4 w-4 text-white" />
+                {title}
+            </Button>
+        </Link>
     )
 }
--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -1,70 +1,37 @@
-import { Button } from "@/components/ui/button";
+'use client';
 import Container from "../Container";
-import Link from "next/link";
 import Image from "next/image";
-import { MapPinCheckInside } from 'lucide-react';
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import ButtonUse from "../Button";
 
-export async function Hero() {
+export default function Hero() {
+
+
     return (
         <div className="relative w-full h-screen">
             <Image src="/7.webp" alt="hero" width={1920} height={1080}
-            className="w-full h-full object-cover bg-opacity-85">
+                className="w-full h-full object-cover bg-opacity-85">
             </Image>
             <Container>
                 <div className="absolute w-full h-screen top-0 left-0">
-                    <Button variant={"outline"}
-                    className="absolute bottom-[24%] left-[22%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link className="text-white text-lg" href="/Market">Supermercado</Link>
-                    </Button>
+                    <ButtonUse title="Supermercado" href="/Market" className="bottom-[24%] left-[22%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute top-[24%] left-[25%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white"/>
-                        <Link href="/" className="text-white text-lg">Hoteis</Link>
-                    </Button>
+                    <ButtonUse title="Hoteis" href="/" className="top-[24%] left-[25%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute bottom-[75%] left-[48%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link href="/" className="text-white text-lg">Hospitais</Link>
-                    </Button>
+                    <ButtonUse title="Hospitais" href="/" className="bottom-[75%] left-[48%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute bottom-[41%] left-[45%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link href="/" className="text-white text-lg">Restaurante</Link>  
-                    </Button>
+                    <ButtonUse title="Restaurante" href="/" className="bottom-[41%] left-[45%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute top-[43%] left-[57%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link href="/" className="text-white text-lg">Industria</Link>
-                    </Button>
+                    <ButtonUse title="Industria" href="/" className="top-[50%] left-[55%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute top-[50%] left-[15%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link href="/House" className="text-white text-lg">Casas</Link>
-                    </Button>
+                    <ButtonUse title="Casas" href="/House" className="top-[50%] left-[15%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute bottom-[70%] right-[26%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link href="/" className="text-white text-lg">Universidade</Link>
-                    </Button>
+                    <ButtonUse title="Universidade" href="/" className="absolute bottom-[70%] right-[26%]" />
 
-                    <Button variant={"outline"}
-                    className="absolute bottom-[80%] right-[16%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className="h-4 w-4 text-white" />
-                        <Link href="/" className="text-white text-lg">Pos Obras</Link>
-                    </Button>
-                    
-                    <Button variant={"outline"}
-                    className="absolute bottom-[30%] right-[2%] bg-zinc-600 opacity-90 hover:bg-zinc-700 hover:opacity-100">
-                        <MapPinCheckInside className=" h-4 w-4 text-white" />
-                        <Link href="/" className="text-white text-lg">Shopping</Link>
-                    </Button> 
+                    <ButtonUse title="Pós Obra" href="/" className="absolute bottom-[80%] right-[16%]" />
+
+                    <ButtonUse title="Shopping" href="/" className="absolute bottom-[30%] right-[2%]" />
                 </div>
             </Container>
         </div>
