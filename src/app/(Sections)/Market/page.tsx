eecbb0606a16c0e69e37d3d797327af83cab4b47
jhionathan <jhionathanb@outlook.com>
2025-03-05 10:25:31 -0300

    edits

--- src/app/(Sections)/Market/page.tsx
+++ src/app/(Sections)/Market/page.tsx
@@ -1,3 +1,4 @@
+import CardUse from "@/app/components/Card";
 import Container from "@/app/components/Container";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
@@ -8,11 +9,13 @@ export default function Market() {
     return (
        <Container>
         <div className="min-h-screen flex flex-col items-center w-full">
-            <div className="my-32">
+            <div className="my-24">
                 <h2 className="text-4xl font-normal">Supermercado</h2>
             </div>
-            <div className="flex gap-4 items-center">
-                <Card className="flex items-center flex-col">
+            <div className="grid grid-cols-3 gap-4">
+                <CardUse title="Padaria" image="/supermarket/Padaria.jpg" href="/Market/Bakery"/>
+                <CardUse title="Açougue" image="/supermarket/Açougue.jpg" href="/Market/Butcher"/>
+                {/* <Card className="flex items-center flex-col">
                     <CardHeader>
                         <h3 className="text-center text-lg">Padaria</h3>
                     </CardHeader>
@@ -59,7 +62,7 @@ export default function Market() {
                             />
                         </Link>
                     </CardContent>
-                </Card>
+                </Card> */}
             </div>
         </div>
        </Container>
--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -15,6 +15,7 @@ interface CardProps {
 export default function CardUse({ title, image, href }: CardProps) {
 
     const cardRef = useRef(null);
+    const cardS = useRef(null);
 
     useEffect(() => {
         const card = cardRef.current
@@ -32,6 +33,22 @@ export default function CardUse({ title, image, href }: CardProps) {
         }
         )
     }, [])
+
+    useEffect(() => {
+        const cards = cardS.current
+
+        gsap.to(cards, {
+            duration: 1,
+            y: 100,
+            stagger: {
+                amount: 1,      // Tempo total para todos os elementos
+                from: "center", // Ponto de origem (center, end, edges, random)
+                grid: [5, 3],   // Para layouts em grade
+                ease: "power2.inOut"
+            }
+        }
+        )
+    })
 
     return (
         <Card ref={cardRef}>
