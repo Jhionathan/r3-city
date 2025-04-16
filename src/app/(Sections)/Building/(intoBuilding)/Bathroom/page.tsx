d6b1410860f856f98643263d732bccda0c2f4163
jhionathan <jhionathanb@outlook.com>
2025-04-16 09:53:13 -0300

    build

--- src/app/(Sections)/Building/(intoBuilding)/Bathroom/page.tsx
+++ src/app/(Sections)/Building/(intoBuilding)/Bathroom/page.tsx
@@ -1,19 +1,101 @@
-import ButtonUse from "@/app/components/Button";
+'use client'
+
+import ButtonUse from "@/app/components/ButtonOnClick";
 import Container from "@/app/components/Container";
 import Image from "next/image";
+import { useRef, useState, useEffect } from "react";
+
+interface Product {
+    id: number
+    codprod: string
+    nome: string
+    categoria: string
+}
 
 export default function Bathroom() {
+
+    const [selectedSection, setSelectedSection] = useState("torneira-inox-banheiro-construcao")
+    const [products, setProducts] = useState<Product[]>([])
+    const [isLoading, setIsLoading] = useState(false)
+    const productsRef = useRef<HTMLDivElement>(null)
+
+    const handleButtonClick = (section: string) => {
+        const normalizedSection = section.trim().toUpperCase()
+        setSelectedSection(normalizedSection)
+        productsRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            setIsLoading(true)
+            console.log("Buscando produtos para categoria:", selectedSection)
+            try {
+                const response = await fetch(`/api/products?categoria=${selectedSection}`)
+                console.log("Status da resposta:", response.status)
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar produtos')
+                }
+                const data = await response.json()
+                console.log("Dados recebidos:", data)
+                setProducts(data)
+            } catch (error) {
+                console.error("Erro ao carregar produtos:", error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchProducts()
+    }, [selectedSection])
+
     return (
         <div className="min-h-screen relative">
-            <Image src="/building/bathroom.jpeg" alt="banheiro" width={1920} height={1080} className="w-full h-full object-cover"/>
+            <Image src="/building/bathroom.jpeg" alt="banheiro" width={1920} height={1080}
+                className="w-full h-full object-cover" />
             <Container>
                 <div className="absolute w-full h-screen top-0 left-0">
-                    <ButtonUse title="Pia em Granito" href="/Building" className="absolute bottom-[28%] left-[25%]" />
-                    <ButtonUse title="Divisoria" href="/Building" className="absolute bottom-[48%] left-[43%]" />
-                    <ButtonUse title="Vaso Sanitario" href="/Building" className="absolute bottom-[28%] left-[59%]" />
-                    <ButtonUse title="Piso Porcelanato" href="/Building" className="absolute bottom-[-5%] left-[40%]" />
-                    <ButtonUse title="Torneira Inox" href="/Building" className="absolute bottom-[35%] left-[75%]" />
-                    <ButtonUse title="Espelho" href="/Building" className="absolute bottom-[58%] left-[24%]" />
+                    <ButtonUse title="Torneira Inox" onClick={() => (handleButtonClick("torneira-inox-banheiro-construcao"))} className="bottom-[35%] left-[45%]" />
+                    <ButtonUse title="Vaso Sanitario" onClick={() => (handleButtonClick("vaso-sanitario-banheiro-construcao"))} className="bottom-[25%] left-[25%]" />
+                    <ButtonUse title="Piso Porcelanato" onClick={() => (handleButtonClick("piso-porcelanato-banheiro-construcao"))} className="bottom-[5%] left-[40%]" />
+                    <ButtonUse title="Pia em Granito" onClick={() => (handleButtonClick("pia-granito-banheiro-construcao"))} className="bottom-[25%] left-[65%]" />
+                    <ButtonUse title="Divisoria" onClick={() => (handleButtonClick("divisoria-banheiro-construcao"))} className="bottom-[60%] left-[35%]" />
+                    <ButtonUse title="Espelho" onClick={() => (handleButtonClick("espelho-banheiro-construcao"))} className="bottom-[55%] left-[80%]" />
+                </div>
+                <div className="w-full flex flex-col justify-center mb-14" ref={productsRef}>
+                    <div className="w-full flex items-center justify-center mt-10">
+                        <h3 className="text-xl font-semibold capitalize">{selectedSection}</h3>
+                    </div>
+                    <div className="mt-10 grid grid-cols-6 gap-4 mx-auto my-auto">
+                        {isLoading ? (
+                            <div className="col-span-6 text-center">Carregando produtos...</div>
+                        ) : products.length > 0 ? (
+                            products.map((item) => (
+                                <div
+                                    key={item.id}
+                                    className="w-52 h-52 flex items-center justify-center gap-2 flex-col overflow-hidden border bg-white border-slate-200 p-1 rounded-md shadow-md"
+                                >
+                                    <div className="h-36 overflow-hidden w-full flex items-center justify-center">
+                                        <Image
+                                            src={`https://r3suprimentos.agilecdn.com.br/${item.codprod}.jpg`}
+                                            alt={item.nome.toUpperCase()}
+                                            width={120}
+                                            height={120}
+                                            className="hover:scale-95 transition-all duration-500"
+                                        />
+                                    </div>
+                                    <div className="h-14 w-full">
+                                        <p className="text-slate-600 text-xs font-mono text-center">
+                                            {item.nome.toUpperCase()}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div className="col-span-6 text-center">
+                                Nenhum produto encontrado para esta categoria.
+                            </div>
+                        )}
+                    </div>
                 </div>
             </Container>
         </div>
