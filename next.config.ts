dc3e5ba5958de1e9a2d0b81112f39c437de1c6df
jhionathan <jhionathanb@outlook.com>
2025-05-29 10:39:22 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,38 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//         {
+//             protocol: 'https',
+//             hostname: 'r3suprimentos.agilecdn.com.br',
+//             port: '',
+//             pathname: '/**',
+//         },
+//     ],
+//   }
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'r3suprimentos.agilecdn.com.br',
-            port: '',
-            pathname: '/**',
-        },
+      {
+        protocol: 'https',
+        hostname: 'r3suprimentos.agilecdn.com.br',
+        port: '',
+        pathname: '/**',
+      },
     ],
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
